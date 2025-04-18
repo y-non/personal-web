2e58ae293a126074194adca8396b21a390a25971
trankhoadev <trankhoadev@gmail.com>
2025-04-18 10:27:11 +0700

    chore: update favicon and add credit of banner author

--- src/config.ts
+++ src/config.ts
@@ -19,9 +19,9 @@ export const siteConfig: SiteConfig = {
 		src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "JUUUUUBOT_7", // Credit text to be displayed
+			url: "https://www.pixiv.net/en/artworks/129037951", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
@@ -30,11 +30,11 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+			src: "/favicon/favicon-simple.png", // Path of the favicon, relative to the /public directory
+			// theme: "light", // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+			sizes: "32x32", // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		},
 	],
 };
 
